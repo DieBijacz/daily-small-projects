cb65c832e8d21585e168d7b93c49992d383f8292
masta <mastanilewicz@gmail.com>
2021-12-09 22:41:39 +0000

    added route

--- server Side Node API/index.js
+++ server Side Node API/index.js
@@ -1,8 +1,33 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 app.listen(3000, () => {console.log('list 3000')})
 app.use(express.static('public'))
+app.use(express.json({limit: '1mb'}))
 
-app.post('/api', (request, resp) => {
-    console.log(request);
+const positions = []
+const ISSpositions = []
+
+app.post('/api', (request, response) => {
+    //server req data
+    const data = request.body
+    positions.push(data)
+    //and as response sends this
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon,
+    })
+    console.log(positions);
+})
+
+app.post('/ISSdata', (req, res) => {
+    const data = req.body
+    ISSpositions.push(data)
+    res.json({
+        status: 'gitara',
+        latitude: data.lat,
+        longitude: data.lon,
+    })
+    console.log(ISSpositions);
 })
--- server Side Node API/public/map.js
+++ server Side Node API/public/map.js
@@ -1,17 +1,57 @@
 const api = 'https://api.wheretheiss.at/v1/satellites/25544'
 const map = L.map('map').setView([0, 0], 6);
 
-let firstTime = true
-let displayMyCords = false
-
-const myCoordsBtn = document.querySelector('#get-my-coords').addEventListener('click', (e) => {
-    displayMyCords = true
-    getMyCoords()
-    e.target.style.display = 'none'
+const sendCoordsBtn = document.querySelector('#send-my-coords')
+sendCoordsBtn.addEventListener('click', () => {
+    sendMyCoords()
+    sendISSCords()
 })
 
-// MY CORDS
+function sendMyCoords() {    
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(async (pos) => {
+            const lat = pos.coords.latitude;
+            const lon = pos.coords.longitude;
+            const data = { lat, lon }
+            const options = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            };
+            //user sends options with data
+            const resp = await fetch('/api', options)
+            //and gets response from server
+            const json = await resp.json()
+            console.log(json);
+        })
+    }
+} 
+async function sendISSCords() {
+    const resp = await fetch(api)
+    const data = await resp.json()
+    const {latitude, longitude} = data
 
+    const dane = { latitude, longitude}
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dane)
+    }
+    const respp = await fetch('/ISSdata', options)
+    const odp = await respp.json()
+    console.log(odp);
+}
+
+
+
+// let firstTime = true
+// let displayMyCords = false
+
+// const myCoordsBtn = document.querySelector('#get-my-coords').addEventListener('click', (e) => {
+//     displayMyCords = true
+//     getMyCoords()
+//     e.target.style.display = 'none'
+// })
 
 // MARKER
 const issIcon = L.icon({
@@ -32,7 +72,7 @@ async function getData() {
     const resp = await fetch(api)
     const data = await resp.json()
     const {latitude, longitude} = data
-    
+
     marker.setLatLng([latitude, longitude])
     map.setView([latitude, longitude], map.getZoom())
     // if (firstTime) {
@@ -43,22 +83,24 @@ async function getData() {
     .innerHTML = `
     ISS latitude: ${latitude.toFixed(2)}°${latitude > 0 ? 'N' : 'S'} </br> 
     IS longitude: ${longitude.toFixed(2)}°${longitude > 0 ? 'E' : 'W'}`
-}
-function displayMyCoords(pos) {
-    document.querySelector('#my-cords')
-    .innerHTML = `
-    My latitude: ${pos.coords.latitude.toFixed(2)}°${pos.coords.latitude > 0 ? 'N' : 'S'} </br> 
-    My longitude: ${pos.coords.longitude.toFixed(2)}°${pos.coords.longitude > 0 ? 'E' : 'W'}`
-}
 
-function getMyCoords() {
-    if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition((pos) => {
-            const myPosition = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map);
-            displayMyCoords(pos)
-        })
-    }
+    return (latitude, longitude)
 }
+// function displayMyCoords(pos) {
+//     document.querySelector('#my-cords')
+//     .innerHTML = `
+//     My latitude: ${pos.coords.latitude.toFixed(2)}°${pos.coords.latitude > 0 ? 'N' : 'S'} </br> 
+//     My longitude: ${pos.coords.longitude.toFixed(2)}°${pos.coords.longitude > 0 ? 'E' : 'W'}`
+// }
+
+// function getMyCoords() {
+//     if ('geolocation' in navigator) {
+//         navigator.geolocation.getCurrentPosition((pos) => {
+//             const myPosition = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map);
+//             displayMyCoords(pos)
+//         })
+//     }
+// }
 
 
 getData()
