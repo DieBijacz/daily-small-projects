cd10ba326668170c39394202dc44634d4d850d05
masta <mastanilewicz@gmail.com>
2022-06-22 16:44:53 +0100

    init shopping cart

---
+++ reactShopCart/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { About } from './pages/About'
+
+function App() {
+  return <Router>
+    <Container>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Container>
+  </Router>
+}
+
+export default App
